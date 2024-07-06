5b87cdeb0b197bfc61da4a73d1d04a87dd1b6513
Andrey Bagrievich <gbdron@gmail.com>
2024-07-06 20:47:24 +0300

    BallsExperiment implementation

--- src/scala/scala3/homework/homework1.scala
+++ src/scala/scala3/homework/homework1.scala
@@ -1,18 +1,40 @@
 package homework
-/*
-class BallsExperiment {
 
-  def isFirstBlackSecondWhite(): Boolean = {
-    ???
-  }
+import scala.util.Random
+
+class BallsExperiment {
+    // набор шаров
+    private var balls = 0 :: 0 :: 0 :: 1 :: 1 :: 1 :: Nil
+
+    // функция вынимает случайный шар из корзины
+    private def getRandomAndRemove(): Int = {
+        val index = Random.nextInt(balls.length)
+        val ball = balls(index)
+        balls = balls.take(index) ++ balls.drop(index + 1)
+        
+        return ball
+    }
+
+    // показать оставшиеся шары в корзине
+    def showBalls{
+        println(balls.mkString(", "))
+    }
+
+    def isFirstBlackSecondWhite(): Option[Boolean] = {
+        if (balls.length < 2) return None
+
+        Some(getRandomAndRemove == 0 && getRandomAndRemove == 1)
+        
+    }
 }
 
 object BallsTest {
   def main(args: Array[String]): Unit = {
     val count = 10000
-    val listOfExperiments: List[BallsExperiment] = ???
-    val countOfExperiments = ???
-    val countOfPositiveExperiments: Float = countOfExperiments.count(_ == true)
+    val listOfExperiments: List[BallsExperiment] = List.fill(count)(new BallsExperiment)
+    val countOfExperiments = listOfExperiments.map(_.isFirstBlackSecondWhite())
+    val countOfPositiveExperiments: Float = countOfExperiments.count(_ == Some(true))
     println(countOfPositiveExperiments / count)
+
   }
-}*/
+}
